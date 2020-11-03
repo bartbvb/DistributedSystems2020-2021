3900ba3cc762c1b517147ddd037374fc7e57d18e
Wouter <woutervmertens@gmail.com>
2020-11-03 11:50:09 +0100

    Update Main.java

--- Java EE 1/CarRental-client/src/java/client/Main.java
+++ Java EE 1/CarRental-client/src/java/client/Main.java
@@ -1,9 +1,16 @@
 package client;
 
+import java.util.Date;
+import java.util.List;
 import javax.ejb.EJB;
+import javax.naming.InitialContext;
+import rental.CarType;
+import rental.Reservation;
+import rental.ReservationConstraints;
+import session.ManagerSessionRemote;
 import session.ReservationSessionRemote;
 
-public class Main {
+public class Main extends AbstractTestAgency<ReservationSessionRemote, ManagerSessionRemote>{
     
     @EJB
     static ReservationSessionRemote session;
@@ -12,5 +19,48 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("found rental companies: "+session.getAllRentalCompanies());
+    }
+
+    public Main(String scriptFile) {
+        super(scriptFile);
+    }
+
+    @Override
+    protected ReservationSessionRemote getNewReservationSession(String name) throws Exception {
+        InitialContext context = new InitialContext();
+        return (ReservationSessionRemote) context.lookup(ReservationSessionRemote.class.getName());
+    }
+
+    @Override
+    protected ManagerSessionRemote getNewManagerSession(String name) throws Exception {
+        InitialContext context = new InitialContext();
+        return (ManagerSessionRemote) context.lookup(ManagerSessionRemote.class.getName());
+    }
+
+    @Override
+    protected void getAvailableCarTypes(ReservationSessionRemote session, Date start, Date end) throws Exception {
+        for(CarType c : session.getAvailableCarTypes(start, end)){
+            System.out.println(c.toString());
+        }
+    }
+
+    @Override
+    protected void createQuote(ReservationSessionRemote session, String name, Date start, Date end, String carType, String region) throws Exception {
+        session.createQuote(name, new ReservationConstraints(start,end,carType,region));
+    }
+
+    @Override
+    protected List<Reservation> confirmQuotes(ReservationSessionRemote session, String name) throws Exception {
+        return session.confirmQuotes();
+    }
+
+    @Override
+    protected int getNumberOfReservationsBy(ManagerSessionRemote ms, String clientName) throws Exception {
+        return ms.getNrOfReservationsByClient(clientName);
+    }
+
+    @Override
+    protected int getNumberOfReservationsForCarType(ManagerSessionRemote ms, String carRentalName, String carType) throws Exception {
+        return ms.getNrOfReservationsForCarType(carRentalName, carType);
     }
 }

89636f41bd1e73c6f75eaab173efe6b14c26ec5b
bartbvb <bart.vanbommel@gmail.com>
2020-11-10 05:52:48 -0500

    changed reservation to entity

--- Java EE 2/CarRental-lib/src/rental/Reservation.java
+++ Java EE 2/CarRental-lib/src/rental/Reservation.java
@@ -1,17 +1,27 @@
 package rental;
 
+import static javax.persistence.CascadeType.REMOVE;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "reservations")
 public class Reservation extends Quote {
-    @Id protected Integer ReservationId;
+    @ManyToOne(cascade=REMOVE)
     private int carId;
+    @Id
+    private Long id;
     
     /***************
      * CONSTRUCTOR *
      ***************/
 
+    public Reservation(){
+    super(null, null,null,null,null,0);
+    }
+    
     public Reservation(Quote quote, int carId) {
     	super(quote.getCarRenter(), quote.getStartDate(), quote.getEndDate(), 
     		quote.getRentalCompany(), quote.getCarType(), quote.getRentalPrice());
@@ -21,9 +31,12 @@ public class Reservation extends Quote {
     /******
      * ID *
      ******/
-    
     public int getCarId() {
     	return carId;
+    }
+    
+    public void setCarId(int carId){
+    this.carId = carId;
     }
     
     /*************
@@ -35,4 +48,12 @@ public class Reservation extends Quote {
         return String.format("Reservation for %s from %s to %s at %s\nCar type: %s\tCar: %s\nTotal price: %.2f", 
                 getCarRenter(), getStartDate(), getEndDate(), getRentalCompany(), getCarType(), getCarId(), getRentalPrice());
     }	
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }

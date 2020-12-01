ed9592b0c5e1f4ff19183fba7ce3b9667b2ba2c8
Bart van Bommel <bart.vanbommel@gmail.com>
2020-12-01 11:58:35 +0100

    added documentation for url of workers

--- GAE 2/src/main/java/ds/gae/Worker.java
+++ GAE 2/src/main/java/ds/gae/Worker.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class Worker extends HttpServlet {
+	
+	//worker is bound to URL: "http://localhost:8080/worker"
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {

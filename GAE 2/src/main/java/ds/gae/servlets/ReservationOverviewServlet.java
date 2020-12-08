2005938a59b03fbf9c8178ca738ce6da1bfe819e
Bart van Bommel <bart.vanbommel@gmail.com>
2020-12-08 11:14:22 +0100

    added servlet classes

---
+++ GAE 2/src/main/java/ds/gae/servlets/ReservationOverviewServlet.java
@@ -0,0 +1,24 @@
+package ds.gae.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ds.gae.view.JSPSite;
+
+@SuppressWarnings("serial")
+public class ReservationOverviewServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        if (req.getSession().getAttribute("renter") == null) {
+            String email = "test.user@kuleuven.be";
+            req.getSession().setAttribute("renter", email);
+        }
+        resp.sendRedirect(JSPSite.PERSIST_TEST.url());
+    }
+}
